3c57f9f5118a56ae4bff45258d45b56e089b8b7a
Sharvi Sethi <43950347+sharvi1203@users.noreply.github.com>
2021-01-12 14:47:25 +0530

    Initial commit

---
+++ src/com/flipkart/app/ApplicationDemo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipkart.app;
+
+/**
+ * @author Dell
+ *
+ */
+public class ApplicationDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
